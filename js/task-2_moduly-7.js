d18f8a7ad85d5a427da94f8b0b7bf3a8e4570967
VolodymyrKoch <67268447+VolodymyrKoch@users.noreply.github.com>
2020-10-16 01:10:06 +0300

    2 task 0.8

---
+++ js/task-2_moduly-7.js
@@ -0,0 +1,26 @@
+// В JS є масив рядків.
+const ingredients = [
+  'Картопля',
+  'Гриби',
+  'Часник',
+  'Помідори',
+  'Зелень',
+  'Приправи',
+];
+// Напиши скрипт, який для кожного елемента масиву ingredients створить окремий li,
+//   після чого вставить всі li за одну операцію в список ul.ingredients.
+// Для створення DOM - вузлів використовуй document.createElement()
+// ------------------------
+const listIngredients = document.querySelector("#ingredients");
+// console.log(listIngredients);
+const cyclicList = ingredients.forEach((el, i, arr) => {
+  const creationItem = document.createElement("li");
+  creationItem.textContent = el;
+
+  // cyclicList.append(creationItem);
+  // return listIngredients;
+    // console.log(el);
+    console.log(creationItem);
+});
+// cyclicList()
+console.log(cyclicList);
