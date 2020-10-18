e7036bed6538b5ac80edfdf380d5948b3976b71c
VolodymyrKoch <67268447+VolodymyrKoch@users.noreply.github.com>
2020-10-19 00:08:50 +0300

    8 start

--- js/task-8_moduly-7.js
+++ js/task-8_moduly-7.js
@@ -9,16 +9,54 @@
 //     Кожен наступний div після першого, повинен бути ширше і вище попереднього на   10px
 // Створи функцію destroyBoxes(), яка очищає div#boxes.
 
-{/* <div id="controls">
-  <input type="number" min="0" max="100" step="1" />
-  <button type="button" data-action="render">Створити</button>
-  <button type="button" data-action="destroy">Очистити</button>
-</div>
+// {/* <div id="controls">
+//   <input type="number" min="0" max="100" step="1" />
+//   <button type="button" data-action="render">Створити</button>
+//   <button type="button" data-action="destroy">Очистити</button>
+// </div>
 
-<div id="boxes"></div> */}
+// <div id="boxes"></div> */}
 
 const controlsRef = document.querySelector('#controls');
-const numberRef = document.querySelector('[number]');
+console.log(controlsRef);
+const numberRef = controlsRef.querySelector('input');
 const buttonRenderRef = document.querySelector('[data-action="render"]');
 const buttonDestroyRef = document.querySelector('[data-action="destroy"]');
-console.log(object);
+console.log(numberRef);
+const boxesRef = document.querySelector('#boxes');
+
+let n = Number(numberRef);
+let nom = 0;
+let createDiv;
+// const box = function (n) {
+//   for (let i = 0; i < n; i += 1) { 
+//     // nom = n[i]
+//     // createDiv = controlsRef.innerHTML += `"<div>Box${nom}<div>"`;
+//     createDiv = controlsRef.innerHTML += "<div>Box<div>";
+//   }
+// };
+
+buttonDestroyRef.addEventListener("click", (e) => {
+  numberRef.value = 0;
+  const delDiv = controlsRef.removeChild(createDiv);
+ });
+
+buttonRenderRef.addEventListener("click", (e) => {
+  // box()
+  createDiv = controlsRef.innerHTML += "<div>Box<div>";
+   });
+
+
+
+
+
+// ------
+// const listIngredients = document.querySelector("#ingredients");
+// console.log(listIngredients);
+// const cyclicList = ingredients.forEach((el, i, arr) => {
+//   const creationItem = document.createElement("li");
+//   creationItem.textContent = el;
+//   listIngredients.appendChild(creationItem)
+  
+//   console.log(creationItem);
+//   });
